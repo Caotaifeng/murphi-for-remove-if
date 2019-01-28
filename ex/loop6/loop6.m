f7bb192c7d88b35e16d21256947ed32b2552dc33
Caotaifeng <32268153+Caotaifeng@users.noreply.github.com>
2019-01-28 16:12:23 +0800

    Add files via upload

---
+++ ex/loop6/loop6.m
@@ -0,0 +1,35 @@
+type number : -5..5;
+	number2 : -10..10;
+var size : number2;
+	copy : number2;
+	available : number2;
+	res_size : number2;
+	res_copy : number2;
+	res_and : number2;
+	
+ruleset m : number;n : number;k : number do startstate "Init"
+begin
+    size := m;
+	copy := n;
+	available := k;
+	res_size := 1;
+	res_copy := m + n;
+	res_and := m + n;
+endstartstate;endruleset;
+
+rule "loop"
+	size  > 0 ==> begin
+	if(available > size) then
+		copy := copy + available;
+		size := size - available;
+	end;
+	endrule;
+
+rule "assert"
+	size <= 0 ==> begin
+	res_size := size;
+	res_copy := copy;
+	endrule;
+
+invariant "main"
+  res_size != 0 | res_copy = res_and;	
---
+++ ex/loop6/loop6n.m
@@ -0,0 +1,56 @@
+
+const
+
+
+type
+
+  number : -5..5;
+  number2 : -10..10;
+
+var
+
+  size : number2;
+  copy : number2;
+  available : number2;
+  res_size : number2;
+  res_copy : number2;
+  res_and : number2;
+
+rule "assert1"
+size <= 0
+==>
+Begin
+  res_size := size;
+  res_copy := copy;
+endrule;
+
+rule "loop2"
+  size > 0 &
+  available > size
+==>
+Begin
+  copy := copy + available;
+  size := size - available;
+endrule;
+
+rule "loop3"
+  size > 0 &
+  available <= size
+==>
+Begin
+endrule;
+
+ruleset  m : number; n : number; k : number do
+startstate
+  size := m;
+  copy := n;
+  available := k;
+  res_size := 1;
+  res_copy := m + n;
+  res_and := m + n;
+endstartstate;
+endruleset;
+invariant "main"
+  (res_size != 0 |
+  res_copy = res_and);
+
